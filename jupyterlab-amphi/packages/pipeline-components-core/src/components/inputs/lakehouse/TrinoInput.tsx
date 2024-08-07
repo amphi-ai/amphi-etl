9b4c8ca3304bb13769dbc23406ee5e46ff52fc72
Anh Anh 95 <107619230+bui-theanh@users.noreply.github.com>
2024-08-07 08:34:46 +0700

    commit iceberg

--- jupyterlab-amphi/packages/pipeline-components-core/src/components/inputs/lakehouse/TrinoInput.tsx
+++ jupyterlab-amphi/packages/pipeline-components-core/src/components/inputs/lakehouse/TrinoInput.tsx
@@ -4,31 +4,39 @@ import { BaseCoreComponent } from '../../BaseCoreComponent';
 
 export class TrinoInput extends BaseCoreComponent {
   constructor() {
-    const defaultConfig = { host: "localhost", port: "3306", databaseName: "", username: "", password: "", tableName: "" };
+    const defaultConfig = { host: "localhost", port: "8082",catalogs : "", schemaName: "", username: "", password: "", tableName: "" };
     const form = {
       fields: [
         {
           type: "input",
           label: "Host",
           id: "host",
-          placeholder: "Enter database host",
-          connection: 'Mysql',
+          placeholder: "Enter Trino host",
+          connection: 'Trino',
           advanced: true
         },
         {
           type: "input",
           label: "Port",
           id: "port",
-          placeholder: "Enter database port",
-          connection: 'Mysql',
+          placeholder: "Enter Trino port",
+          connection: 'Trino',
           advanced: true
         },
         {
           type: "input",
-          label: "Database Name",
-          id: "databaseName",
-          placeholder: "Enter database name",
-          connection: 'Mysql',
+          label: "Catalogs",
+          id: "catalogs",
+          placeholder: "Enter Trino Catalogs",
+          connection: 'Trino',
+          advanced: true
+        },
+        {
+          type: "input",
+          label: "Schema Name",
+          id: "schemaName",
+          placeholder: "Enter Trino schema name",
+          connection: 'Trino',
           advanced: true
         },
         {
@@ -36,7 +44,7 @@ export class TrinoInput extends BaseCoreComponent {
           label: "Username",
           id: "username",
           placeholder: "Enter username",
-          connection: "Mysql",
+          connection: "Trino",
           advanced: true
         },
         {
@@ -45,7 +53,7 @@ export class TrinoInput extends BaseCoreComponent {
           id: "password",
           placeholder: "Enter password",
           inputType: "password",
-          connection: "Mysql",
+          connection: "Trino",
           advanced: true
         },
         {
@@ -67,30 +75,34 @@ export class TrinoInput extends BaseCoreComponent {
       ],
     };
 
-    super("Trino Input-Test", "trinoInput", "pandas_df_input", [], "inputs.Lakehouse", trinoIcon, defaultConfig, form);
+    super("Trino Input", "trinoInput", "pandas_df_input", [], "inputs.Lakehouse", trinoIcon, defaultConfig, form);
   }
   
     public provideImports({config}): string[] {
-      return ["import pandas as pd", "import sqlalchemy", "import pymysql"];
+      return ["import pandas as pd", "import sqlalchemy", "import trino"];
     }  
 
     public generateComponentCode({ config, outputName }): string {
-      let connectionString = `mysql+pymysql://${config.username}:${config.password}@${config.host}:${config.port}/${config.databaseName}`;
+      let connectionString = `trino.dbapi.connect(
+                                      host='${config.host}',
+                                      port=${config.port},
+                                      user='${config.username}',
+                                      catalog='${config.catalogs}',
+                                      schema='${config.schemaName}')`;
       const uniqueEngineName = `${outputName}_Engine`; // Unique engine name based on the outputName
       const sqlQuery = config.sqlQuery && config.sqlQuery.trim() ? config.sqlQuery : `SELECT * FROM ${config.tableName}`;
       const code = `
-# Connect to the MySQL database
-${uniqueEngineName} = sqlalchemy.create_engine("${connectionString}")
+
+conn = ${connectionString}
 
 # Execute SQL statement
 try:
-    with ${uniqueEngineName}.connect() as conn:
-        ${outputName} = pd.read_sql(
-            """${sqlQuery}""",
-            con=conn.connection
-        ).convert_dtypes()
+    cursor = conn.cursor() # Connect to the Trino database
+    cursor.execute('${sqlQuery}')
+    ${uniqueEngineName} = pd.DataFrame(cursor.fetchall(), columns=[desc[0] for desc in cursor.description])
+    
 finally:
-    ${uniqueEngineName}.dispose()
+    cursor.close()
 `;
       return code;
     }
