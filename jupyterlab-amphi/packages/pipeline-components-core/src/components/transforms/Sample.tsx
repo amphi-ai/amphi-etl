843447ced4a84b41e8aec57bc651370e217dde27
tgourdel <github@t.gourdel.fr>
2024-11-01 09:39:24 -0700

    Improved sample

--- jupyterlab-amphi/packages/pipeline-components-core/src/components/transforms/Sample.tsx
+++ jupyterlab-amphi/packages/pipeline-components-core/src/components/transforms/Sample.tsx
@@ -2,20 +2,38 @@ import { ComponentItem, PipelineComponent } from '@amphi/pipeline-components-man
 import { randomIcon } from '../../icons';
 import { BaseCoreComponent } from '../BaseCoreComponent';
 
-
-
 export class Sample extends BaseCoreComponent {
   constructor() {
-    const defaultConfig = { mode: "random" };
+    const defaultConfig = { numberType: "number", rows: 1, percentage: 1, mode: "random", groupBy: [] };
     const form = {
       idPrefix: "component__form",
       fields: [
+        {
+          type: "radio",
+          label: "Type",
+          id: "numberType",
+          options: [
+            { value: "number", label: "Fixed Number" },
+            { value: "percentage", label: "Percentage" }
+          ],
+          advanced: true
+        },
         {
           type: "inputNumber",
           label: "Rows number",
           id: "rows",
           placeholder: "0",
-          min: 0
+          min: 0,
+          condition: { numberType: "number" }
+        },
+        {
+          type: "inputNumber",
+          label: "Percentage",
+          id: "percentage",
+          placeholder: "0",
+          min: 0,
+          max: 100,
+          condition: { numberType: "percentage" }
         },
         {
           type: "radio",
@@ -27,10 +45,17 @@ export class Sample extends BaseCoreComponent {
             { value: "tail", label: "Last" }
           ],
           advanced: true
+        },
+        {
+          type: "columns",
+          label: "Group By Columns",
+          id: "groupBy",
+          selectAll: true,
+          advanced: true
         }
       ],
     };
-    const description = "Use the Sample to limit data by selecting a specified number of rows, either randomly, from the start or from the end of the dataset.";
+    const description = "Use the Sample component to limit data by selecting a specified number of rows or percentage, either randomly, from the start, or from the end of the dataset. You can also group the sampling by one or more columns.";
 
     super("Sample Datasets", "sample", description, "pandas_df_processor", [], "transforms", randomIcon, defaultConfig, form);
   }
@@ -42,12 +67,46 @@ export class Sample extends BaseCoreComponent {
   public generateComponentCode({ config, inputName, outputName }): string {
     let sampleCode = "";
 
-    if (config.mode === "random") {
-      sampleCode = `${outputName} = ${inputName}.sample(n=${config.rows})`;
-    } else if (config.mode === "tail") {
-      sampleCode = `${outputName} = ${inputName}.tail(${config.rows})`;
-    } else if (config.mode === "head") {
-      sampleCode = `${outputName} = ${inputName}.head(${config.rows})`;
+    if (config.numberType === "number") {
+      if (config.mode === "random") {
+        if (config.groupBy.length > 0) {
+          sampleCode = `${outputName} = ${inputName}.groupby([${config.groupBy.map(col => `"${col.value}"`).join(', ')}]).sample(n=${config.rows})`;
+        } else {
+          sampleCode = `${outputName} = ${inputName}.sample(n=${config.rows})`;
+        }
+      } else if (config.mode === "tail") {
+        if (config.groupBy.length > 0) {
+          sampleCode = `${outputName} = ${inputName}.groupby([${config.groupBy.map(col => `"${col.value}"`).join(', ')}]).tail(${config.rows})`;
+        } else {
+          sampleCode = `${outputName} = ${inputName}.tail(${config.rows})`;
+        }
+      } else if (config.mode === "head") {
+        if (config.groupBy.length > 0) {
+          sampleCode = `${outputName} = ${inputName}.groupby([${config.groupBy.map(col => `"${col.value}"`).join(', ')}]).head(${config.rows})`;
+        } else {
+          sampleCode = `${outputName} = ${inputName}.head(${config.rows})`;
+        }
+      }
+    } else if (config.numberType === "percentage") {
+      if (config.mode === "random") {
+        if (config.groupBy.length > 0) {
+          sampleCode = `${outputName} = ${inputName}.groupby([${config.groupBy.map(col => `"${col.value}"`).join(', ')}]).sample(frac=${config.percentage / 100})`;
+        } else {
+          sampleCode = `${outputName} = ${inputName}.sample(frac=${config.percentage / 100})`;
+        }
+      } else if (config.mode === "tail") {
+        if (config.groupBy.length > 0) {
+          sampleCode = `${outputName} = ${inputName}.groupby([${config.groupBy.map(col => `"${col.value}"`).join(', ')}]).apply(lambda x: x.tail(int(len(x) * ${config.percentage / 100}))).reset_index(drop=True)`;
+        } else {
+          sampleCode = `${outputName} = ${inputName}.iloc[-int(len(${inputName}) * ${config.percentage / 100}):]`;
+        }
+      } else if (config.mode === "head") {
+        if (config.groupBy.length > 0) {
+          sampleCode = `${outputName} = ${inputName}.groupby([${config.groupBy.map(col => `"${col.value}"`).join(', ')}]).apply(lambda x: x.head(int(len(x) * ${config.percentage / 100}))).reset_index(drop=True)`;
+        } else {
+          sampleCode = `${outputName} = ${inputName}.iloc[:int(len(${inputName}) * ${config.percentage / 100})]`;
+        }
+      }
     }
 
     // Template for the pandas query code
@@ -56,7 +115,4 @@ ${sampleCode}
 `;
     return code;
   }
-
-
-
 }
