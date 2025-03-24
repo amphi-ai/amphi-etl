0d370160421d45d311c8c0de874cdfb9d479a5cb
tgourdel <github@t.gourdel.fr>
2025-03-23 22:26:13 -0700

    Bump to v0.8.24

--- amphi-etl/setup.py
+++ amphi-etl/setup.py
@@ -23,15 +23,15 @@ data_files = (
 
 setup(
     name='amphi-etl',
-    version='0.8.23',
+    version='0.8.24',
     description='Open-source and Python-based ETL',
     author='Thibaut Gourdel',
     author_email='tgourdel@amphi.ai',
     license='ELv2',
     install_requires=[
-        'jupyterlab==4.3.3',
-        'jupyterlab-amphi==0.8.23',
-        'pandas==2.2.1'
+        'jupyterlab==4.3.6',
+        'jupyterlab-amphi==0.8.24',
+        'pandas==2.2.3'
     ],
     keywords=[],  # Added an empty list for keywords to resolve the dynamic 'keywords' issue.
     packages=find_packages(include=['amphi', 'amphi.theme-light', 'amphi.ui-component', 'config', 'packages']),  # Custom package discovery.
